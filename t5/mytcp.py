33641b869bc40f92f6be251bb204e01b886bbec0
LorhanSohaky <lorhansohaky@gmail.com>
2019-12-21 16:06:28 -0300

    Arrumada mensagens de debug e fechamento de conexão

--- t5/mytcp.py
+++ t5/mytcp.py
@@ -5,7 +5,7 @@ import math
 import time
 import struct
 
-DEBUG = False
+DEBUG = True
 
 
 class Servidor:
@@ -31,13 +31,16 @@ class Servidor:
             # Ignora segmentos que não são destinados à porta do nosso servidor
             return
 
-        pseudohdr = str2addr(src_addr) + str2addr(dst_addr) + \
-            struct.pack('!HH', 0x0006, len(segment))
-        if calc_checksum(pseudohdr + segment) != 0:
-            print('descartando segmento com checksum incorreto')
-            return
+	try:
+            pseudohdr = str2addr(src_addr) + str2addr(dst_addr) + \
+                struct.pack('!HH', 0x0006, len(segment))
+            if calc_checksum(pseudohdr + segment) != 0:
+                print('descartando segmento com checksum incorreto')
+                return
 
-        payload = segment[4*(flags>>12):]
+       if (flags & FLAGS_FIN) == FLAGS_FIN:
+                self.conexoes.pop(id_conexao)
+ payload = segment[4*(flags>>12):]
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
@@ -48,8 +51,6 @@ class Servidor:
         elif id_conexao in self.conexoes:
             # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
-            if (flags & FLAGS_FIN) == FLAGS_FIN:
-                self.conexoes.pop(id_conexao)
         else:
             print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
                   (src_addr, src_port, dst_addr, dst_port))
@@ -78,6 +79,7 @@ class Conexao:
         self.nao_enviados = b''
         self.janela = 1
         self.last_seq = None
+        self.closing = False
 
         self._start_connection()
 
@@ -89,7 +91,7 @@ class Conexao:
         self.seq_no += 1
         self.send_base = self.seq_no
         if DEBUG:
-            print(src_addr, 'connected with', dst_addr)
+            print(f'{src_addr}:{src_port}', 'connected with', f'{dst_addr}:{dst_port}')
             print('handshaking: seq->',self.seq_no,'ack->',self.ack_no)
 
     def _start_timer(self):
@@ -117,7 +119,7 @@ class Conexao:
             _, _, dst_addr, _ = self.id_conexao
             print(dst_addr, 'retransmiting: seq->', self.send_base, 'ack->', self.ack_no,
                       'bytes->', len(msg), ('timer-> %.3f' % self.timeout_interval) if self.timer is None else ('timer -> anterior'))
-            self._send_ack_segment(msg)
+        self._send_ack_segment(msg)
 
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
@@ -126,7 +128,7 @@ class Conexao:
         
         if self.ack_no == seq_no:
             if DEBUG:
-                print(src_addr, 'receiving: seq->', seq_no, 'ack->', ack_no, 'bytes->', len(payload))
+                print(f'{src_addr}:{src_port}', 'receiving: seq->', seq_no, 'ack->', ack_no, 'bytes->', len(payload))
 
             if ack_no > self.send_base and (flags & FLAGS_ACK) == FLAGS_ACK:
                 self.nao_confirmados = self.nao_confirmados[ack_no-self.send_base:]
@@ -146,17 +148,25 @@ class Conexao:
 
             if self.last_seq == ack_no:
                 self.janela += 1
-                print('Updated window', self.janela, 'MSS')
+                if DEBUG:
+                    print('Updated window', self.janela, 'MSS')
                 self._send_pending()
 
             self.retransmitindo = False
             self.ack_no += len(payload)
 
-            if (flags & FLAGS_FIN) == FLAGS_FIN:
+            if (flags & FLAGS_FIN) == FLAGS_FIN and not self.closing:
+                self.closing = True
                 self.ack_no += 1 # É preciso somar, pois quando é enviada a flag FIN não há payload
-                flags = FLAGS_FIN | FLAGS_ACK
+                cabecalho = make_header(dst_port, src_port,self.seq_no,self.ack_no,FLAGS_FIN | FLAGS_ACK)
+                cabecalho = fix_checksum(cabecalho, src_addr, dst_addr)
+                self.servidor.rede.enviar(cabecalho, dst_addr)
+                return
 
-            if ((flags & FLAGS_FIN) == FLAGS_FIN) or len(payload) > 0: # Só Deus sabe, mas funciona
+            elif (flags & FLAGS_ACK) == FLAGS_ACK and self.closing:
+                self.servidor.conexoes.pop(self.id_conexao)
+
+            if len(payload) > 0: # Só Deus sabe, mas funciona
                 dados = make_header(src_port, dst_port, self.seq_no, self.ack_no, flags)
                 dados = fix_checksum(dados, src_addr,dst_addr)
                 self.servidor.rede.enviar(dados,src_addr)
@@ -180,23 +190,25 @@ class Conexao:
         self.nao_enviados = self.nao_enviados[(self.janela * MSS):]
 
         self.last_seq = self.seq_no + len(prontos_para_envio)
-        print( 'Last seq', self.last_seq)
+        if DEBUG:
+            print( 'Last seq', self.last_seq)
 
         numero_de_segmentos = math.ceil(len(prontos_para_envio) / MSS)
         if numero_de_segmentos == 0: # Caso seja uma mensagem em que o tamanho dos dados seja < MSS
             numero_de_segmentos = 1
         for i in range(numero_de_segmentos):
             msg = prontos_para_envio[i*MSS:(i+1)*MSS]
-            if DEBUG:
-                if self.timer is None:
+            if self.timer is None:
+                if DEBUG:
                     print('++Timer started++')
-                _, _, dst_addr, _ = self.id_conexao
-                print(dst_addr, 'sending: seq->', self.seq_no, 'ack->', self.ack_no,
+                    _, _, dst_addr, _ = self.id_conexao
+                    print(dst_addr, 'sending: seq->', self.seq_no, 'ack->', self.ack_no,
                         'bytes->', len(msg), ('timer-> %.3f' % self.timeout_interval) if self.timer is None else ('timer -> anterior'))
             self._send_ack_segment(msg)
 
     def _send_pending(self):
-        print('Enviando pendentes')
+        if DEBUG:
+            print('Enviando pendentes')
         tamanho_pendentes = (self.janela * MSS ) - len(self.nao_confirmados)
 
         if tamanho_pendentes > 0:
@@ -205,19 +217,20 @@ class Conexao:
                 return
             self.nao_enviados = self.nao_enviados[tamanho_pendentes:]
             self.last_seq = self.seq_no + len(prontos_para_envio)
-            print('Last seq', self.last_seq)
+            if DEBUG:
+                print('Last seq', self.last_seq)
 
             numero_de_segmentos = math.ceil(len(prontos_para_envio) / MSS)
             if numero_de_segmentos == 0:  # Caso seja uma mensagem em que o tamanho dos dados seja < MSS
                 numero_de_segmentos = 1
             for i in range(numero_de_segmentos):
                 msg = prontos_para_envio[i*MSS:(i+1)*MSS]
-                if DEBUG:
-                    if self.timer is None:
+                if self.timer is None:
+                    if DEBUG:
                         print('++Timer started++')
-                    _, _, dst_addr, _ = self.id_conexao
-                    print(dst_addr, 'sending: seq->', self.seq_no, 'ack->', self.ack_no,
-                        'bytes->', len(msg), ('timer-> %.3f' % self.timeout_interval) if self.timer is None else ('timer -> anterior'))
+                        _, _, dst_addr, _ = self.id_conexao
+                        print(dst_addr, 'sending: seq->', self.seq_no, 'ack->', self.ack_no,
+                            'bytes->', len(msg), ('timer-> %.3f' % self.timeout_interval) if self.timer is None else ('timer -> anterior'))
                 self._send_ack_segment(msg)
 
 
@@ -267,7 +280,7 @@ class Conexao:
         self.timeout_interval = self.estimated_rtt + 4 * self.dev_rtt
         if DEBUG:
             print('New timeout %.3f' % self.timeout_interval)
-    
+
     def __str__(self):
         src_addr, src_port, _, _ = self.id_conexao
         return f'{src_addr}:{src_port}'
